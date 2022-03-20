8931583f042709ee431ad513833fb1486fc0cb1b
RambabuMS <raambabu96@gmail.com>
2022-03-21 00:21:54 +0530

    new app

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 //const express = require("express");// "type" : "commonjs",
 import express from "express"; // "type" : "module"
 import { MongoClient } from "mongodb";
+import { moviesRouter } from "./routes/movies.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import {
-  getMoviesById,
-  createMovies,
-  updateMovieById,
-  deleteMovieById,
-  getAllMovies,
-} from "./helper.js";
+import bcrypt from "bcrypt";
+
 dotenv.config();
 
 console.log(process.env);
@@ -106,50 +102,13 @@ app.get("/", function (request, response) {
   response.send("Hello World");
 });
 
-// find - cursor - pagination -> convert to Array - toArray();
-
-app.get("/movies", async function (request, response) {
-  //response.send(movies);
-  const movies = await getAllMovies();
-  response.send(movies);
-});
-
-app.get("/movies/:id", async function (request, response) {
-  console.log(request.params);
-  const { id } = request.params;
-  //const movie = movies.find((mv) => mv.id === id);
-  const movie = await getMoviesById(id);
-  console.log(movie);
-  movie
-    ? response.send(movie)
-    : response.status(404).send({ message: "No Such movie found" });
-});
-
-app.delete("/movies/:id", async function (request, response) {
-  console.log(request.params);
-
-  const { id } = request.params;
-  //const movie = movies.find((mv) => mv.id === id);
-  const result = await deleteMovieById(id);
-  response.send(result);
-});
-
-app.put("/movies/:id", async function (request, response) {
-  console.log(request.params);
-  //db.movies.updateOne({ id : id}, {$set : updateData})
-  const { id } = request.params;
-  const updateData = request.body;
-  //const movie = movies.find((mv) => mv.id === id);
-  const result = await updateMovieById(id, updateData);
-  response.send(result);
-});
-
-app.post("/movies", async function (request, response) {
-  // db.movies.insertMany(data)
-  const data = request.body;
-  console.log(data);
-  const result = await createMovies(data);
-  response.send(result);
-});
+app.use("/movies", moviesRouter);
 
 app.listen(PORT, () => console.log("Server is started in ", PORT));
+
+async function genPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  console.log({ salt, hashPassword });
+}
+genPassword("password@123");
---
+++ routes/movies.js
@@ -0,0 +1,56 @@
+import express from "express";
+const router = express.Router();
+import {
+  createMovies,
+  getAllMovies,
+  getMoviesById,
+  updateMovieById,
+  deleteMovieById,
+} from "../helper.js";
+
+// find - cursor - pagination -> convert to Array - toArray();
+router.get("/", async function (request, response) {
+  //response.send(movies);
+  const movies = await getAllMovies();
+  response.send(movies);
+});
+
+router.get("/:id", async function (request, response) {
+  console.log(request.params);
+  const { id } = request.params;
+  //const movie = movies.find((mv) => mv.id === id);
+  const movie = await getMoviesById(id);
+  console.log(movie);
+  movie
+    ? response.send(movie)
+    : response.status(404).send({ message: "No Such movie found" });
+});
+
+router.delete("/:id", async function (request, response) {
+  console.log(request.params);
+
+  const { id } = request.params;
+  //const movie = movies.find((mv) => mv.id === id);
+  const result = await deleteMovieById(id);
+  response.send(result);
+});
+
+router.put("/:id", async function (request, response) {
+  console.log(request.params);
+  //db.movies.updateOne({ id : id}, {$set : updateData})
+  const { id } = request.params;
+  const updateData = request.body;
+  //const movie = movies.find((mv) => mv.id === id);
+  const result = await updateMovieById(id, updateData);
+  response.send(result);
+});
+
+router.post("/", async function (request, response) {
+  // db.movies.insertMany(data)
+  const data = request.body;
+  console.log(data);
+  const result = await createMovies(data);
+  response.send(result);
+});
+
+export const moviesRouter = router;
